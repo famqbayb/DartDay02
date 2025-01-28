21419478a629b7ec64843ba6be6c04d8768afa06
famqbayb <franckmorelassogba@gmail.com>
2025-01-28 14:06:10 +0100

    determination longueur du message

---
+++ exo_1/protocole_alpha.dart
@@ -0,0 +1,26 @@
+
+
+void main() { 
+ String longueur = """Cher agent X,
+
+
+
+J'ai aperçu le chat noir    au point de rendez-vous 7. L'agent Johnson était 
+
+également présent.    Le code secret 42 a été activé.
+
+
+
+Notre agent de liaison vous contactera à 23h.
+
+
+
+    Cordialement""";
+  
+  int longueurCalcul = longueur.length;
+  print(longueurCalcul);
+  
+  
+  
+   }
+      
